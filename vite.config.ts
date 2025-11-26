5f4ad2d8694c0ebfa0e82e2f220257e1ef487fd9
Olya-Hello <olgayurchikova10@gmail.com>
2025-11-26 19:10:12 +1000

    fixed name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'superTaskLite'
+const repoName = 'tasklite'
 
 export default defineConfig({
 plugins: [react()],
