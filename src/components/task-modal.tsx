84e9b136ce5fb994f57482dbcca559ca7da09fb7
Olya-Hello <olgayurchikova10@gmail.com>
2025-12-01 21:40:39 +1000

    fixed modal window

--- src/components/task-modal.tsx
+++ src/components/task-modal.tsx
@@ -16,7 +16,7 @@ const ModalBack = styled.div<{visable: boolean}>`
 `;
 
 const Modal = styled.div<{visible: boolean}>`
-    min-width: ${p => p.theme.spacing(48)};
+    min-width: ${p => p.theme.spacing(38)};
     background-color: ${p => p.theme.colors.surface};
     padding: 
     ${p => p.theme.spacing(2.7)};
